0df8dd7e1275c93c6aadeb533324e8c4e54edeba
binl233 <835741923@qq.com>
2024-02-24 11:12:38 -0500

    Add continuous extract feature

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import extractor
 
 def main():
-    path = input("Folder path: ")
-    currRate = int(input("Current frame rate: "))
-    targetRate = int(input("Target frame rate: "))
-
-
-    while currRate <= targetRate:
-        print("Target frame rate must larger than Current frame rate.")
+    while True:
+        path = input("Folder path: ")
         currRate = int(input("Current frame rate: "))
         targetRate = int(input("Target frame rate: "))
-    
-    print("Start extracting...")
 
-    op = extractor.extract(path, currRate, targetRate)
 
-    if op:
-        print("Success!")
-    else:
-        print("Failed")
+        while currRate <= targetRate:
+            print("Target frame rate must larger than Current frame rate.")
+            currRate = int(input("Current frame rate: "))
+            targetRate = int(input("Target frame rate: "))
+        
+        print("Start extracting...")
 
-    print("Press any key to exit")
-    input()
+        op = extractor.extract(path, currRate, targetRate)
+
+        if op:
+            print("Success!\n")
+        else:
+            print("Failed\n")
 
 if __name__ == "__main__":
     main()
